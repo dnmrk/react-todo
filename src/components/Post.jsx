74b8f7c3c4b13acdbac75e5a2400c03617dd8236
dnmrk <dean@abovethefray.io>
2026-03-04 15:07:09 +0800

    46. Dynamic Routes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,14 @@
+
+import { Link } from 'react-router-dom';
 import classes from './Post.module.css';
 
-function Post(props) {
+function Post({ id, author, body }) {
   return (
     <div className={classes.post}>
-      <p className={classes.author}>{props.author}</p>
-      <p className={classes.text}>{props.body}</p>
+      <Link to={id}>
+        <p className={classes.author}>{author}</p>
+        <p className={classes.text}>{body}</p>
+      </Link>
     </div>
   );
 }
